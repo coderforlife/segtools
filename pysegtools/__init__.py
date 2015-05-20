4b2a35592b4ccb30f5f0a4a1f0383fbe813fb425
Jeffrey Bush <jeff@coderforlife.com>
2015-05-20 15:32:26 -0700

    Made it so failed imports during loading "imstack" don't completely prevent it from actually loading.

--- pysegtools/__init__.py
+++ pysegtools/__init__.py
@@ -3,10 +3,16 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-# The following don't work, but after we have the rough equivalent
-#from . import general
-#from . import images
-#from . import tasks
-__import__(__name__+'.general', globals(), locals())
-__import__(__name__+'.images', globals(), locals())
-#__import__(__name__+'.tasks', globals(), locals())
+try: # this try-except is to allow --check to work without requiring imports to work
+    # The following don't work, but after we have the rough equivalent
+    #from . import general
+    #from . import images
+    #from . import tasks
+    __import__(__name__+'.general', globals(), locals())
+    __import__(__name__+'.images', globals(), locals())
+    #__import__(__name__+'.tasks', globals(), locals())
+except ImportError as ex:
+    # We will still warn though
+    from warnings import warn
+    warn(ex.message, RuntimeWarning, 2)
+    del warn
